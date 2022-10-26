2c645250a572ef21ee9edd03b6ddc5267511800a
Kellbber <kellbberbarcarolo@gmail.com>
2022-10-26 14:42:32 -0300

    feat(project): start project

---
+++ src/assets/styles/colors.tsx
@@ -0,0 +1,12 @@
+export const colors = {
+    primaryColor:"#ffffff",
+
+    secundaryColor:"#D9D9D9",
+    
+
+    backgroundColor:"42444A",
+
+    textColor:"rgba(0,0,0,0.7)",
+    
+    detailColor:"#e4142c",
+}
---
+++ src/assets/styles/constants.tsx
@@ -0,0 +1,15 @@
+export const constants = {
+
+  headingFontFamily: "Montserrat",
+  headingFontSize: "20px",
+  headingLineHeight: "140%",
+
+  inputFontFamily: "Lexend Deca",
+  inputFontSize: "10px",
+  inputLineHeight: 800,
+
+  checkboxFontFamily:"Inter",
+  checkboxFontSize:" 15px",
+  checkboxLineHeight:400,
+  
+};
