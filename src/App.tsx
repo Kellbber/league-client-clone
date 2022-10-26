acfd7811c495d455f20e67d5888311b517393ab2
Kellbber <kellbberbarcarolo@gmail.com>
2022-10-26 14:42:52 -0300

    feat(style): implemented style components

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+import {ThemeProvider} from 'styled-components'
+import {BrowserRouter} from 'react-router-dom';
+
+import './App.css'
+
+function App() {
+
+
+  return (
+    <BrowserRouter>
+
+    </BrowserRouter>
+  )
+}
+
+export default App;
